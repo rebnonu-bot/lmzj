4b0497f9650dca00ae4eb0c33e54ed74de31a3c9
LIUZEHUI <liuzehui@LiuZehuideMacBook-Pro.local>
2026-02-08 14:29:15 +0800

    chore: 添加 .gitignore 并从 Git 中移除 node_modules

--- node_modules/@tdesign/uniapp-chat/dist/types/chat-sender.d.ts
+++
@@ -1,7 +0,0 @@
-import type { TransformEventHandlers, ExtractNonOnProps } from '@tdesign/uniapp/common/common';
-import type { TdChatSenderProps } from '../chat-sender/type';
-
-export type ChatSenderProps = ExtractNonOnProps<TdChatSenderProps>;
-export type ChatSenderEmits = TransformEventHandlers<TdChatSenderProps, true>;
-declare const ChatSenderComponent: import('vue').DefineComponent<ChatSenderProps, {}, {}, {}, {}, {}, {}, ChatSenderEmits, any>;
-export default ChatSenderComponent;
