148d8edb8175cf203a3a64561078dde433637b19
LIUZEHUI <liuzehui@LiuZehuideMacBook-Pro.local>
2026-02-10 13:52:41 +0800

    chore: 调整仓库策略，仅保留 dist 和 README

---
+++ dist/build/h5/assets/config.BxfTPpYJ.js
@@ -0,0 +1,1 @@
+const e={isMock:!0,baseUrl:"",placeholderImage:"https://img.js.design/assets/img/65a8e1a1b16c905330e6a8e8.png"};export{e as c};
--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  isMock: true,
-  baseUrl: '',
-  // 系统统一占位图
-  placeholderImage: 'https://img.js.design/assets/img/65a8e1a1b16c905330e6a8e8.png',
-};
